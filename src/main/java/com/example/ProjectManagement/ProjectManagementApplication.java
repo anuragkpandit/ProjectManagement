dad490099fd2f167ff61f6642a0ea7db32564863
Anurag Pandit <anuragkpandit22@gmail.com>
2023-08-22 18:53:00 +0530

    first commit

--- src/main/java/com/example/ProjectManagement/ProjectManagementApplication.java
+++ src/main/java/com/example/ProjectManagement/ProjectManagementApplication.java
@@ -5,13 +5,19 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @OpenAPIDefinition(info = @Info(title = "Employees API", version = "2.0", description = "Employees Information"))
-public class ProjectManagementApplication {
+public class ProjectManagementApplication{
 
+	/*@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(ProjectManagementApplication.class);
+	}*/
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementApplication.class, args);
 	}
